1b7ec830caa951afa5610289ec4713bca24ad0a2
Charles Yuan <charlesyuan314@gmail.com>
2015-09-26 16:07:12 -0400

    old history code

--- client/views/history/history.js
+++ client/views/history/history.js
@@ -1,16 +1,6 @@
 var TempStore = GIBE.collections.TempStore;
-
-var image = {
-    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/IPhone_5.png/96px-IPhone_5.png",
-    caption: "iphone"
-    }
-
-var image2 = {
-    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/IPhone_5.png/96px-IPhone_5.png",
-    caption: "cloudsight"
-    }
-
-
 Template.history.helpers({
-  images: [image, image2]
-});
+  images: function () {
+    return TempStore.find().fetch();
+  }
+ });
