b00f1eaca18f689f0f47798c66f9b6501cf754a9
Mark Lee <marklee.lhs@gmail.com>
2015-09-25 22:27:54 -0400

    Add untested meteor method

--- server/server.js
+++ server/server.js
@@ -1,5 +1,40 @@
+var helpers = GIBE.helpers;
+var Future = Meteor.npmRequire('fibers/future');
+
 Meteor.methods({
   'searchForImage': function (data) {
-    console.log(data);
+    check(data, String);
+
+    var future = new Future();
+    var postOptions = {
+      headers: {
+        'Authorization': 'CloudSight ' + helpers.getSetting('cloudSightKey')
+      },
+      params: {
+        'image_request[image]': helpers.base64ToBuffer(data),
+        'image_request[locale]': 'en-US'
+      }
+    };
+
+    HTTP.post('https://api.cloudsightapi.com/image_requests', postOptions, function (error, result) {
+      if (error) {
+        throw error;
+      }
+
+      var getOptions = {
+        headers: {
+          'Authorization': 'CloudSight ' + get.setting('cloudSightKey')
+        }
+      };
+
+      HTTP.get('https://api.cloudsightapi.com/image_responses/' + result.token, getOptions, function (error, result) {
+        if (error) {
+          throw error;
+        }
+        future.return(result);
+      });
+    });
+
+    return future.wait();
   }
 });
