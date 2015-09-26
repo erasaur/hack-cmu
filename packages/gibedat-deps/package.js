8522298ad8288a0db22c51a0f56096ba2a366a77
Mark Lee <marklee.lhs@gmail.com>
2015-09-25 20:24:53 -0400

    Add packages and setup package structure

---
+++ packages/gibedat-deps/package.js
@@ -0,0 +1,30 @@
+Package.describe({
+  name: 'gibedat-deps',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.0.1');
+
+  api.imply([
+    'ecmascript',
+    'check',
+    'ejson',
+    'localstorage',
+    'erasaur:meteor-lodash@3.10.1',
+    'fourseven:scss@3.2.0',
+    'iron:router@1.0.9',
+    'mdg:reload-on-resume',
+    'meteoric:ionic@0.1.19',
+    'meteoric:ionic-sass@0.3.0',
+    'meteoric:ionicons-sass@0.1.7',
+    'reactive-var'
+  ]);
+});
